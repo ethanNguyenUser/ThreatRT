9a4c051bfa1904c52d21bd7865d499338f3089ea
Ethan Nguyen <45704850+ethanNguyenUser@users.noreply.github.com>
2023-11-06 15:07:24 -0800

    Added code

--- Ethan Training/Training/Assets/Scripts/Dropper.cs
+++ Ethan Training/Training/Assets/Scripts/Dropper.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+    [SerializeField] float timeToWait = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<MeshRenderer>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time > timeToWait)
+        {
+
+        }
+
     }
 }
