cc40223b4b0efd7a57505352631f4aaa946c873f
Ethan Nguyen <45704850+ethanNguyenUser@users.noreply.github.com>
2023-11-05 19:19:49 -0800

    More changes to training

--- Ethan Training/Training/Assets/Mover.cs
+++ Ethan Training/Training/Assets/Mover.cs
@@ -7,18 +7,31 @@ public class Mover : MonoBehaviour
     //[SerializeField] float xValue = 0;
     //[SerializeField] float yValue = 0.02f;
     //[SerializeField] float zValue = 0;
+    [SerializeField] float moveSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        PrintInstruction();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal");
-        float zValue = Input.GetAxis("Vertical");
+        MovePlayer();
+    }
+
+    void PrintInstruction()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Test out the collision! (it's a little buggy unfortunately)");
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, 0, zValue);
     }
 }
