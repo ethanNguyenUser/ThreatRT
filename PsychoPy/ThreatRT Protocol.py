4be2d07b826830e93a2f8846a9f77135ab655e8a
Ethan Nguyen <45704850+ethanNguyenUser@users.noreply.github.com>
2023-11-13 21:56:20 -0800

    Finished PsychoPy protocol

--- PsychoPy/ThreatRT Protocol.py
+++ PsychoPy/ThreatRT Protocol.py
@@ -4,9 +4,11 @@ import csv
 import os
 from datetime import datetime
 
-NUM_TRIALS = 1  # Number of trials per stimulus
-MIN_WAIT = 1.5  # Minimum wait time
-MAX_WAIT = 3.5  # Maximum wait time
+TRIALS_PER_STIMULUS = 15  # Number of trials per stimulus
+MIN_WAIT = 1  # Minimum wait time
+MAX_WAIT = 3  # Maximum wait time
+DESIRED_AREA = 0.25  # Adjust this value as needed for image size
+TIME_BETWEEN_TRIALS = 0.5 # Time between trials
 
 # Prompt for participant name
 info = {"Participant Name": ""}
@@ -27,7 +29,7 @@ event.waitKeys(keyList=["space"])
 
 # Stimuli list
 stimuli = ["snake", "spider", "square", "circle", "star", "triangle", "clock", "chair", "apple", "banana"]
-trials = data.TrialHandler(stimuli * NUM_TRIALS, 1, method="random")  # NUM_TRIALS for each stimulus
+trials = data.TrialHandler(stimuli * TRIALS_PER_STIMULUS, 1, method="random")  # NUM_TRIALS for each stimulus
 
 # Crosshair
 crosshair = visual.TextStim(win, text="+", height=0.1)
@@ -38,41 +40,40 @@ if not os.path.exists(data_folder):
     os.makedirs(data_folder)
 
 # Prepare CSV file
-timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-csv_filename = os.path.join(data_folder, f"{participant_name}_reaction_times_{timestamp}.csv")
+timestamp = datetime.now().strftime("%m-%d-%Y_%H_%M_%S")
+csv_filename = os.path.join(data_folder, f"{participant_name}_{timestamp}.csv")
 with open(csv_filename, "w", newline="") as file:
     writer = csv.writer(file)
-    writer.writerow(["Stimulus", "Reaction Time (s)"])
+    writer.writerow(["Stimulus", "Reaction Time (ms)"])
 
     # Main experiment loop
     for trial in trials:
+        # Draw crosshair
         crosshair.draw()
         win.flip()
+        
+        # Wait
         wait_time = random.uniform(MIN_WAIT, MAX_WAIT)  # Random wait between MIN_WAIT and MAX_WAIT
         core.wait(wait_time)
 
-        # Display image
+        # Display image to have DESIRED_AREA
         imagePath = os.path.join("Images", trial + ".png")
-        image = visual.ImageStim(win, image=imagePath, size=(0.5, 0.5))  # Adjust size as needed
+        image = visual.ImageStim(win, image=imagePath)
+        original_size = image.size
+        aspect_ratio = original_size[0] / original_size[1]
+        new_height = (DESIRED_AREA / aspect_ratio) ** 0.5
+        new_width = aspect_ratio * new_height
+        image.size = (new_width, new_height)
         image.draw()
         win.flip()
         timer = core.Clock()
         keys = event.waitKeys(keyList=["space"], timeStamped=timer)
         
-        # Check for premature press
-        if keys[0][1] < wait_time:
-            warning = visual.TextStim(win, text="Please wait for the image to appear before pressing space. This trial will be discounted.", height=0.05)
-            warning.draw()
-            win.flip()
-            core.wait(2)
-            continue  # Skip this trial
-        
         # Record reaction time
-        reaction_time = keys[0][1] - wait_time
+        reaction_time = keys[0][1] * 1000
         writer.writerow([trial, reaction_time])
-
-        # Inter-trial interval
-        core.wait(1)
+        
+        core.wait(TIME_BETWEEN_TRIALS)
 
 # Close the window
 win.close()
